91d709c911fcd820a1c8e2689174a50cb86af96a
reoanrudo <sodapon03@gmail.com>
2026-01-11 10:44:04 +0900

    Add line 5

--- フォーマットテスト.py
+++ フォーマットテスト.py
@@ -1,7 +1,5 @@
 import tkinter as tk
-
-
-  class    Test:
-          def    __init__(self):
-              self  .  x  =  1
-              self.y  =  2
+class Test:
+         def __init__(self):
+        self.x   = 1
+        self.y = 2
